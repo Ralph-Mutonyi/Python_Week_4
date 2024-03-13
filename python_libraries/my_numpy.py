bd217786c992588c8a0187d021d4cbae185ee628
RALPH MUTONYI <ralfmutonyi@gmail.com>
2024-03-13 14:52:07 +0300

    in session

---
+++ python_libraries/my_numpy.py
@@ -0,0 +1,22 @@
+# python libraries
+
+"""
+    Numpy = numerical python
+    consist of single-dimensional and multidimensional array elements
+
+    Computes numerical  data in python
+    used in numerical computation - science and Engineering
+"""
+# Example
+
+# importing an array
+
+import numpy as np
+
+# creating an array using np.array()method
+
+arr = np.array([10, 20, 30, 40, 50])
+
+#printing
+
+print(arr)
